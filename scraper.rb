416694ace2c3cb2e8a34ac6ffebfb1a36d1d7cc1
kelpenhagen <kelpenhagen@gmail.com>
2015-09-04 14:47:31 +1000

    chagnes

--- scraper.rb
+++ scraper.rb
@@ -1,13 +1,17 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+
+# 
+agent = Mechanize.new
 #
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("http://www.parliament.nsw.gov.au/prod/parlment/nswbills.nsf/V3BillsListAll")
+page.at(:table).search(:tr)[2].search(:td)[0].at(:a)[:href]
+
 #
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
